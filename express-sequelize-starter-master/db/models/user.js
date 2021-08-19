51f41bd16b74dca934a5013bd1c95d3bb02739a1
grantc00 <grantcky@gmail.com>
2021-08-19 15:00:59 -0700

    phase 5ing

--- express-sequelize-starter-master/db/models/user.js
+++ express-sequelize-starter-master/db/models/user.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Tweet, {
+        as: "tweets",
+        foreignKey: "userId",
+      });
     }
   };
   User.init({
